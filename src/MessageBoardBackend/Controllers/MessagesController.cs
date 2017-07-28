7e0bc239d82fa566425cc44b1185ed138098da8e
hassan.adel <hasanadel555@hotmail.com>
2017-07-28 20:41:24 +0200

    Returning the message after posting

--- src/MessageBoardBackend/Controllers/MessagesController.cs
+++ src/MessageBoardBackend/Controllers/MessagesController.cs
@@ -37,11 +37,18 @@ namespace MessageBoardBackend.Controllers
             return "value";
         }
 
+        /*
+         *  We first need to get our backend to respond with the new message that was just added. 
+         *  The reason for doing this is that in most cases where you use a database you will generate
+         *  a new ID for each item you add and so you pass back the item you just added which contains
+         *  that new ID.
+         */
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]Message message)
+        public Message Post([FromBody]Message message)
         {
             messages.Add(message);
+            return message;
         }
 
         // PUT api/values/5

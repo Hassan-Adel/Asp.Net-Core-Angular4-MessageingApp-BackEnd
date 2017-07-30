a2daffa5d5a0a462349642b9b65fee58f2b8b93d
hassan.adel <hasanadel555@hotmail.com>
2017-07-30 03:28:45 +0200

    Adding Authorization Controller with JWT

---
+++ src/MessageBoardBackend/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using MessageBoardBackend.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessageBoardBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        public class JwtPacket
+        {
+            public string Token{ get; set; }
+        }
+
+        // POST api/values
+        [HttpPost("register")]
+        public JwtPacket Register([FromBody]User user)
+        {
+            var jwt = new JwtSecurityToken();
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return new JwtPacket() { Token = encodedJwt };
+        }
+        
+    }
+}

07b992b0e46f0db3f447c36c5c1cefc0b9155a55
hassan.adel <hasanadel555@hotmail.com>
2017-08-01 12:22:58 +0200

    Adding UsersController

---
+++ src/MessageBoardBackend/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MessageBoardBackend.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private ApiContext context;
+
+        public UsersController(ApiContext _context)
+        {
+            this.context = _context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult Get(string id)
+        {
+            var user = context.Users.SingleOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound("User not found");
+
+            return Ok(user);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
